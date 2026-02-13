e6e18012832acdf6f6560716bf0c82365c3fbb36
Ilya <northenlight6899@gmail.com>
2026-02-13 17:42:45 +0300

    remove data.py

--- flask_anime_api/data.py
+++
@@ -1,9 +0,0 @@
-from uuid import UUID
-
-anime_data = [
-    {'id': UUID('caa6865d-14f0-4fd0-82df-6f157cc3ac26'), 'title': 'Cowboy Bebop', 'episodes': 24},
-    {'id': UUID('3a218cb9-6bb6-4ab5-9347-a55fa3e44e76'), 'title': 'Lain', 'episodes': 12},
-    {'id': UUID('a837c19c-73c0-4890-8ad3-c70cb34b0664'), 'title': 'Eva', 'episodes': 23},
-    {'id': UUID('0ee64eb2-1a12-49f9-8c77-883d36f7c0c8'), 'title': 'Frieren', 'episodes': 24},
-    {'id': UUID('3f06fcb8-f2e2-411e-ac93-75c3c22a382c'), 'title': 'Naruto', 'episodes': 220},
-]
