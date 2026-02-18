6ced24efca9bb9b3bcb50839201af570c07805d3
Ilya <northenlight6899@gmail.com>
2026-02-18 19:12:12 +0300

    migration script

---
+++ alembic/versions/9d1d7a21b0af_anime_type_column.py
@@ -0,0 +1,29 @@
+"""anime Type column
+
+Revision ID: 9d1d7a21b0af
+Revises: 
+Create Date: 2026-02-18 18:18:47.014811
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9d1d7a21b0af'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column(
+        'anime',
+        sa.Column('type', sa.String, nullable=True)
+    )
+
+
+def downgrade() -> None:
+    op.drop_column('anime','type')
