8b71f875984308d60f88ef503531c81cbfa4b33b
Ilya <northenlight6899@gmail.com>
2026-02-21 16:23:16 +0300

    add secret key

--- flask_anime_api/config.py
+++ flask_anime_api/config.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY')
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_HOST = os.getenv('DB_HOST')
