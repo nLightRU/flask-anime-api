32402ff93c99261b2df99aa0990f1724f2c056ea
Ilya <northenlight6899@gmail.com>
2026-02-24 22:09:22 +0300

    fix PUT anime

--- flask_anime_api/anime/repository.py
+++ flask_anime_api/anime/repository.py
@@ -50,22 +50,27 @@ class AnimeRepository:
                 if hasattr(data, attr) and getattr(a, attr) != getattr(data, attr):
                     update_values[attr] = getattr(data, attr)
                     
-            if update_values != {}:
+            # If no base fields like title or episodes is updated
+            # update querry is not executed
+            if update_values:
                 sess.execute(
                     update(Anime)
                     .where(Anime.id == id_)
                     .values(**update_values)
                 )
+                sess.refresh(a)
 
+            # Check for studio updates
             stmt = select(Studio).where(Studio.id.in_(data.studios_ids))
             new_studios = set(sess.scalars(stmt).all())
-            a.studios = new_studios
+            if a.studios != new_studios:
+                print('studios updated')
+                a.studios = new_studios
+                sess.refresh(a)
             
-            sess.refresh(a)
-            
-            studios_ids = [s.id for s in a.studios]
+            studios = [BaseEntityInList(id=s.id, name=s.name) for s in a.studios]
 
-            return AnimeDTO(**a.to_dict(), studios_ids=studios_ids)
+            return AnimeDTO(**a.to_dict(), studios=studios)
         
     def delete(self, id_):
         with self.database.session_scope() as s:
--- flask_anime_api/anime/service.py
+++ flask_anime_api/anime/service.py
@@ -10,19 +10,12 @@ class AnimeService:
         self.anime_repo = AnimeRepository()
         self.studios_repo = StudioRepository()
 
-    def __get_anime_studios(self, a: AnimeDTO, studios: list[StudioDTO]) -> list[BaseEntityInList]:
-        data = []
-        for s in studios:
-            if s.id in a.studios_ids:
-                data.append(BaseEntityInList(id=s.id, name=s.name))
-        return data
-
     def get_all(self) -> list[AnimeResponseScheme]:
         try:
             anime = self.anime_repo.get_all()
         except:
             raise
-                
+
         data = []
         for a in anime:
             data.append(AnimeResponseScheme(**a.model_dump()))
@@ -34,22 +27,15 @@ class AnimeService:
             a = self.anime_repo.get_by_id(id_)
         except:
             raise
-        
+
         if (a is None) or a.is_deleted:
             return None
-        
+
         return AnimeResponseScheme(**a.model_dump())
-    
+
     def update_anime(self, anime_id: UUID, anime_data: AnimeUpdateScheme) -> AnimeResponseScheme:
         a = self.anime_repo.update(anime_id, anime_data)
         if not a:
             return None
-        
-        studios = self.studios_repo.get_all()
 
-        return AnimeResponseScheme(
-            id=a.id,
-            title=a.title,
-            episodes=a.episodes,
-            studios=[s for s in self.__get_anime_studios(a, studios)]
-        )
+        return AnimeResponseScheme(**a.model_dump())
